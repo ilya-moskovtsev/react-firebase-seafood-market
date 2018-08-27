a2f86ed3e2f6cee36e6d0d70f814c3dd19e492c1
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2018-08-27 22:03:51 +0300

    Stateless Functional Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,91 @@
 import React from "react";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h1>Catch
-                    <span className="ofThe">
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>Catch
+//                     <span className="ofThe">
+//                         <span className="of">of</span>
+//                         <span className="the">the</span>
+//                     </span>
+//                     Day</h1>
+//                 <h3 className="tagline">
+//                     {/*passing dynamic data with props*/}
+//                     <span>{this.props.tagline}</span>
+//                 </h3>
+//             </header>
+//         );
+//     };
+// }
+
+// Stateless Functional Component
+// function Header(props) {
+//     return (
+//         <header className="top">
+//             <h1>Catch
+//                 <span className="ofThe">
+//                         <span className="of">of</span>
+//                         <span className="the">the</span>
+//                     </span>
+//                 Day</h1>
+//             <h3 className="tagline">
+//                 {/*passing dynamic data with props*/}
+//                 <span>{props.tagline}</span>
+//             </h3>
+//         </header>
+//     );
+// }
+
+// Stateless Functional Component
+// arrow function with implicit return
+// const Header = (props) => (
+//     <header className="top">
+//         <h1>Catch
+//             <span className="ofThe">
+//                         <span className="of">of</span>
+//                         <span className="the">the</span>
+//                     </span>
+//             Day</h1>
+//         <h3 className="tagline">
+//             {/*passing dynamic data with props*/}
+//             <span>{props.tagline}</span>
+//         </h3>
+//     </header>
+// );
+
+// Stateless Functional Component
+// arrow function with one argument doesn't need parentheses
+// const Header = props => (
+//     <header className="top">
+//         <h1>Catch
+//             <span className="ofThe">
+//                         <span className="of">of</span>
+//                         <span className="the">the</span>
+//                     </span>
+//             Day</h1>
+//         <h3 className="tagline">
+//             {/*passing dynamic data with props*/}
+//             <span>{props.tagline}</span>
+//         </h3>
+//     </header>
+// );
+
+// Stateless Functional Component
+// object destructuring
+const Header = ({tagline}) => (
+    <header className="top">
+        <h1>Catch
+            <span className="ofThe">
                         <span className="of">of</span>
                         <span className="the">the</span>
                     </span>
-                    Day</h1>
-                <h3 className="tagline">
-                    {/*passing dynamic data with props*/}
-                    <span>{this.props.tagline}</span>
-                </h3>
-            </header>
-        );
-    };
-}
+            Day</h1>
+        <h3 className="tagline">
+            {/*passing dynamic data with props*/}
+            <span>{tagline}</span>
+        </h3>
+    </header>
+);
 
 export default Header;
