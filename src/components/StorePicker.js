648dca0a9c7b747e22a0d8c43e57294846dda22d
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2018-09-11 21:50:09 +0300

    Events, Refs and this Binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,6 +2,38 @@ import React from 'react';
 import {getFunName} from "../helpers";
 
 class StorePicker extends React.Component {
+    // #region this Binding
+    // constructor() {
+    //     super();
+    //     console.log("Going to create a component");
+    //     not the greatest way when component has many methods
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+    //
+    // custom methods need binding to access this
+    // goToStore(event) {
+    //     console.log(this);
+    // }
+    //
+    // componentDidMount() {
+    //     console.log("Mounted");
+    //     console.log(this);
+    // }
+    // #endregion
+
+    storeNameInput = React.createRef();
+
+    // don't need to bind this for properties
+    goToStore = event => {
+        // 1. Stop the form from submitting
+        // prevent page refresh
+        event.preventDefault();
+        // 2. Get the text from the input
+        console.log(this.storeNameInput);
+        // 3. Change the page to /store/whatever-they-entered
+    };
+
+
     render() {
         //too complex
         // return React.createElement('p', {
@@ -63,9 +95,13 @@ class StorePicker extends React.Component {
         // );
 
         return (
-            <form action="" className="store-selector">
+            // onSubmit - React SyntheticEvent
+            <form action="" className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please name your store</h2>
-                <input type="text" placeholder="Store Name" defaultValue={getFunName()}></input>
+                <input type="text"
+                       ref={this.storeNameInput}
+                       placeholder="Store Name"
+                       defaultValue={getFunName()}/>
                 <button type="submit">Visit Store</button>
             </form>
         );
